45f38b3ff230d2504b156122925e6fdc3170a99c
Saipranav Kalapala <saipranav1289@gmail.com>
2022-06-13 13:53:24 -0400

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Pranav Kalapala on 2022-06-13.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
